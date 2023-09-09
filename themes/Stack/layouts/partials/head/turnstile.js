f39a05bb68e403552cd9fb06424de8501c82dfe6
WTNLXTBL <WTNLXTBL@outlook.com>
2023-09-09 21:39:56 +0800

    update

--- themes/Stack/layouts/partials/head/turnstile.js
+++
@@ -1,8 +0,0 @@
-window.onloadTurnstileCallback = function () {
-    turnstile.render('#example-container', {
-        sitekey: '0x4AAAAAAAJ7s3uvgVPCFYag',
-        callback: function(token) {
-            console.log(`Challenge Success ${token}`);
-        },
-    });
-};
